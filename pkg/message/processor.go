17e8d09583ee480c27cf28c0520730ee4fc0dc47
Atlas Kaplan <aakaplan@proton.me>
2023-11-19 15:31:19 +0000

    message processor more wip code

--- pkg/message/processor.go
+++ pkg/message/processor.go
@@ -1,13 +1,31 @@
 package message
 
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"twitch_chat_analysis/pkg/rabbitmq"
+)
+
 type Processor struct{}
 
 func NewProcessor() Processor { return Processor{} }
 
-func (p Processor) Proccess() error {
-	// rmq, err := rabbitmq.Get()
-	// if err != nil {
-	// 	return fmt.Errorf("error getting rabbitmq channel: %s", err)
-	// }
-	return nil
+func (p Processor) Proccess() (<-chan amqp.Delivery, error) {
+	rmq, err := rabbitmq.Get()
+	if err != nil {
+		return nil, fmt.Errorf("error getting rabbitmq channel: %s", err)
+	}
+	msgs, err := rmq.Channel.Consume(
+		rmq.Queue.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error consume messages: %s", err)
+	}
+	return msgs, nil
 }
